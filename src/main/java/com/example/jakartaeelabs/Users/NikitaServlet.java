3ab7b5f08fb4f5d873eaee606ba671e759ca40d5
Nikita <mechanis1@gmail.com>
2023-04-27 00:07:28 +0300

    Initial commit

---
+++ src/main/java/com/example/jakartaeelabs/Users/NikitaServlet.java
@@ -0,0 +1,26 @@
+package com.example.jakartaeelabs.Users;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.*;
+
+@WebServlet(name = "nikitaServlet", value = "/nikita")
+public class NikitaServlet extends HttpServlet {
+
+    public void init() {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/templates/nikita.html");
+        dispatcher.include(request, response);
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Users</title>
+  <link rel="stylesheet" href="styles/main-page.css">
+</head>
+<body>
+<img class="header" src="images/jakarta-ee.webp" alt="">
+<h1 class="title">Team 8</h1>
+<div class="users">
+  <a href="nikita">Nikita</a>
+  <a href="vlad">Vlad</a>
+</div>
+</body>
+</html>
